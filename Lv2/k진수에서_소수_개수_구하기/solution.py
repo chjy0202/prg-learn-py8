1b116d775d2bc5c63d353d90c2d8757235ab0c17
chjy0202 <chjy0202@naver.com>
2022-04-03 19:36:37 +0900

    prg_k진수_소수판별수정(시간초과해결)

--- Lv2/k진수에서_소수_개수_구하기/solution.py
+++ Lv2/k진수에서_소수_개수_구하기/solution.py
@@ -1,26 +1,27 @@
 # 소수인지 판별하는 함수
 def is_prime(n):
-    for i in range(2, n // 2):
+    if n == 1:
+        return False
+    # 짝지어 제외시킬 수 있기 때문에 제곱근 까지만 판별하는 것이 이득!
+    for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
             return False
     return True
 
-def solution(n, q):
-    count = 0
-    num_str = ''
-
-    # q진수로 변환하는 방법
+# q 진수로 변환하는 함수
+def trans(n, q):
+    num = ''
     while n > 0:
         n, mod = divmod(n, q)
-        num_str += str(mod)
-
-	  # str을 역으로 리턴하는 방법
-    num_lst = num_str[::-1].split('0')
-
-    num_lst = [v for v in num_lst if v]
-    for n in num_lst:
-        if n != '1' and is_prime(int(n)):
-            count += 1
-    print(num_lst, count)
-    return count
+        num += str(mod)
+    # str을 역으로 리턴하는 방법
+    return num[::-1]
     
+def solution(n, q):
+    count = 0
+    num_lst = trans(n, q).split('0')
+
+    for n in num_lst:
+        if n != '' and is_prime(int(n)):
+            count += 1
+    return count

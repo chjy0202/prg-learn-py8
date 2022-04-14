f618815fa95cc88ee9297e1cf8fba09577f83a8b
chjy0202 <chjy0202@naver.com>
2022-04-14 23:29:32 +0900

    prg_자물쇠와_열쇠_2차solution_언패킹

--- Lv3/자물쇠와_열쇠/solution.py
+++ Lv3/자물쇠와_열쇠/solution.py
@@ -1,11 +1,5 @@
 def rotate(key):
-    n = len(key)
-    m = len(key[0])
-    result = [[0] * m for _ in range(n)]
-    for i in range(n):
-        for j in range(m):
-            result[j][n - i - 1] = key[i][j]
-    return result
+    return [list(reversed(i)) for i in zip(*key)]
 
 def check(new_lock):
     lock_len = len(new_lock) // 3
@@ -15,27 +9,31 @@ def check(new_lock):
                 return False
     return True
     
+def is_unlock(x, y, key, new_lock, m):
+    for i in range(m):
+        for j in range(m):
+            new_lock[x + i][y + j] += key[i][j]
+    if check(new_lock):
+        return True
+    for i in range(m):
+        for j in range(m):
+            new_lock[x + i][y + j] -= key[i][j]
+    return False
+
 def solution(key,lock):
     n = len(lock)
     m = len(key)
-    
-    new_lock = [[0 for _ in range(n * 3)] for _ in range(n *3)]
-    # new_lock = [[0] * (n*3) for _ in range(n*3)]
+    new_lock = [[0] * (n*3) for _ in range(n*3)]
+
     for i in range(n):
         for j in range(n):
             new_lock[n + i][n + j] = lock[i][j]
-    print(new_lock)
     
-    for rotation in range(4):
+    for _ in range(4):
         key = rotate(key)
         for x in range(n - m + 1, n * 2):
             for y in range(n - m + 1, n * 2):
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x + i][y + j] += key[i][j]
-                if check(new_lock):
+                if is_unlock(x, y, key, new_lock, m):
                     return True
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x + i][y + j] -= key[i][j]
+    
     return False
